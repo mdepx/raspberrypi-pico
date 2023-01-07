a0828137ac19e83e1561ca47b37bfcef4991ad0b
Ruslan Bukin <br@bsdpad.com>
2023-01-07 12:18:39 +0000

    Add cyw43 stubs.

---
+++ src/cyw43.c
@@ -0,0 +1,201 @@
+#include <lib/cyw43-driver/src/cyw43.h>
+#include <lib/cyw43-driver/src/cyw43_internal.h>
+#include <lib/cyw43-driver/src/cyw43_spi.h>
+
+void
+cyw43_schedule_internal_poll_dispatch(void (*func)(void))
+{
+
+}
+
+void
+cyw43_post_poll_hook(void)
+{
+
+}
+
+void
+cyw43_cb_tcpip_init(cyw43_t *self, int itf)
+{
+
+}
+
+void
+cyw43_cb_tcpip_deinit(cyw43_t *self, int itf)
+{
+
+}
+
+void
+cyw43_delay_ms(uint32_t ms)
+{
+
+}
+
+void
+cyw43_delay_us(uint32_t us)
+{
+
+}
+
+void
+cyw43_hal_get_mac(int idx, uint8_t buf[6])
+{
+
+}
+
+void
+cyw43_hal_generate_laa_mac(int idx, uint8_t buf[6])
+{
+
+}
+
+uint32_t
+read_reg_u32_swap(cyw43_int_t *self, uint32_t fn, uint32_t reg)
+{
+
+	return (0);
+}
+
+int
+cyw43_spi_init(cyw43_int_t *self)
+{
+
+	return (0);
+}
+
+void cyw43_spi_deinit(cyw43_int_t *self)
+{
+
+}
+
+void
+cyw43_spi_reset(void)
+{
+
+}
+
+void
+cyw43_await_background_or_timeout_us(uint32_t timeout_us)
+{
+
+}
+
+static uint32_t
+_cyw43_read_reg(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint size)
+{
+
+	return (0);
+}
+
+uint32_t
+cyw43_read_reg_u32(cyw43_int_t *self, uint32_t fn, uint32_t reg)
+{
+
+    return _cyw43_read_reg(self, fn, reg, 4);
+}
+
+int
+cyw43_read_reg_u16(cyw43_int_t *self, uint32_t fn, uint32_t reg)
+{
+
+    return _cyw43_read_reg(self, fn, reg, 2);
+}
+
+int
+cyw43_read_reg_u8(cyw43_int_t *self, uint32_t fn, uint32_t reg)
+{
+
+    return _cyw43_read_reg(self, fn, reg, 1);
+}
+
+static inline int
+_cyw43_write_reg(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t val,
+    uint size)
+{
+
+    return (0);
+}
+
+int
+cyw43_write_reg_u32(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t val)
+{
+
+    return _cyw43_write_reg(self, fn, reg, val, 4);
+}
+
+int
+cyw43_write_reg_u16(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint16_t val)
+{
+
+    return _cyw43_write_reg(self, fn, reg, val, 2);
+}
+
+int
+cyw43_write_reg_u8(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t val)
+{
+
+    return _cyw43_write_reg(self, fn, reg, val, 1);
+}
+
+void
+cyw43_cb_tcpip_set_link_up(cyw43_t *self, int itf)
+{
+
+}
+
+void
+cyw43_cb_tcpip_set_link_down(cyw43_t *self, int itf)
+{
+
+}
+
+void
+cyw43_cb_process_ethernet(void *cb_data, int itf, size_t len,
+    const uint8_t *buf)
+{
+
+}
+
+int
+cyw43_read_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len,
+    uint8_t *buf)
+{
+
+	return (0);
+}
+
+int
+cyw43_write_bytes(cyw43_int_t *self, uint32_t fn, uint32_t addr, size_t len,
+    const uint8_t *src)
+{
+
+	return (0);
+}
+
+struct pbuf;
+
+uint16_t
+pbuf_copy_partial(const struct pbuf *p, void *dataptr, uint16_t len,
+    uint16_t offset);
+
+uint16_t
+pbuf_copy_partial(const struct pbuf *p, void *dataptr, uint16_t len,
+    uint16_t offset)
+{
+
+	return (0);
+}
+
+void
+cyw43_spi_gpio_setup(void)
+{
+
+}
+
+int
+write_reg_u32_swap(cyw43_int_t *self, uint32_t fn, uint32_t reg, uint32_t val)
+{
+
+	return (0);
+}
---
+++ src/cyw43_configport.h
@@ -0,0 +1,136 @@
+
+#ifndef _CYW43_CONFIGPORT_H
+#define _CYW43_CONFIGPORT_H
+
+#include <sys/cdefs.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <assert.h>
+
+#ifndef CYW43_HOST_NAME
+#define CYW43_HOST_NAME "Pico-W"
+#endif
+
+#ifndef CYW43_LWIP
+#define CYW43_LWIP	0
+#endif
+
+#ifndef	CYW43_GPIO
+#define	CYW43_GPIO	1
+#endif
+
+#ifndef	CYW43_LOGIC_DEBUG
+#define	CYW43_LOGIC_DEBUG	0
+#endif
+
+#ifndef	CYW43_USE_OTP_MAC
+#define	CYW43_USE_OTP_MAC	1
+#endif
+
+#ifndef	CYW43_NO_NETUTILS
+#define	CYW43_NO_NETUTILS	1
+#endif
+
+#ifndef	CYW43_IOCTL_TIMEOUT_US
+#define	CYW43_IOCTL_TIMEOUT_US	1000000
+#endif
+
+#ifndef	CYW43_USE_STATS
+#define	CYW43_USE_STATS		0
+#endif
+
+#ifndef	CYW43_HAL_MAC_WLAN0
+#define	CYW43_HAL_MAC_WLAN0	0
+#endif
+
+#ifndef	STATIC
+#define	STATIC	static
+#endif
+
+#ifndef	CYW43_USE_SPI
+#define	CYW43_USE_SPI	1
+#endif
+
+#ifndef	CYW43_SPI_PIO
+#define	CYW43_SPI_PIO	1
+#endif
+
+#define	CYW43_EPERM		-1
+#define	CYW43_EIO		-2
+#define	CYW43_EINVAL		-3
+#define	CYW43_ETIMEDOUT		-4
+
+#define	CYW43_NUM_GPIOS		4
+
+#define	CYW43_THREAD_ENTER	critical_enter()
+#define	CYW43_THREAD_EXIT	critical_exit()
+#define	CYW43_THREAD_LOCK_CHECK
+
+#define CYW43_SDPCM_SEND_COMMON_WAIT cyw43_await_background_or_timeout_us(1000);
+#define CYW43_DO_IOCTL_WAIT	cyw43_await_background_or_timeout_us(1000);
+#define CYW43_POST_POLL_HOOK	cyw43_post_poll_hook();
+#define CYW43_ARRAY_SIZE(x)	(sizeof(x) / sizeof((x)[0]))
+
+#define CYW43_PIN_WL_HOST_WAKE	24
+#define	CYW43_PIN_WL_SDIO_1	2
+#define	CYW43_PIN_WL_REG_ON	2
+#define	CYW43_PIN_WL_SDIO	2
+
+#define CYW43_HAL_PIN_MODE_INPUT	(1)
+#define CYW43_HAL_PIN_MODE_OUTPUT	(2)
+#define CYW43_HAL_PIN_PULL_NONE		(0)
+#define CYW43_HAL_PIN_PULL_UP		(1)
+#define CYW43_HAL_PIN_PULL_DOWN		(2)
+
+#define cyw43_hal_pin_obj_t u_int
+typedef uint32_t uint;
+
+void cyw43_schedule_internal_poll_dispatch(void (*func)(void));
+void cyw43_post_poll_hook(void);
+void cyw43_delay_ms(uint32_t ms);
+void cyw43_delay_us(uint32_t us);
+void cyw43_hal_get_mac(int idx, uint8_t buf[6]);
+void cyw43_hal_generate_laa_mac(int idx, uint8_t buf[6]);
+void cyw43_await_background_or_timeout_us(uint32_t timeout_us);
+
+static inline uint32_t
+cyw43_hal_ticks_us(void)
+{
+
+	return 0;
+}
+
+static inline uint32_t
+cyw43_hal_ticks_ms(void)
+{
+
+	return 0;
+}
+
+static inline int
+cyw43_hal_pin_read(cyw43_hal_pin_obj_t pin)
+{
+
+	return 0;
+}
+
+static inline void
+cyw43_hal_pin_low(cyw43_hal_pin_obj_t pin)
+{
+
+}
+
+static inline void
+cyw43_hal_pin_high(cyw43_hal_pin_obj_t pin)
+{
+
+}
+
+static inline void
+cyw43_hal_pin_config(cyw43_hal_pin_obj_t pin, uint32_t mode, uint32_t pull,
+    __unused uint32_t alt)
+{
+
+}
+
+#endif
